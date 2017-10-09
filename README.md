3321055843f6ebb4ecb2a3e38df493e11caec006
Valentin Chatelain <val_92_@hotmail.fr>
2017-10-09 22:05:06 +0200

    Added api for Supreme products Fix 404 Update README

--- README.md
+++ README.md
@@ -9,3 +9,11 @@ Returns an array of object representing upcoming and previous drops.
 ### /drops/:drop-slug/products
 
 Returns every products released for a drop.
+
+### /categories
+
+List the available categories of products in the Supreme Shop.
+
+### /categories/:category/products
+
+List all products for the specified category.
--- api.js
+++ api.js
@@ -5,7 +5,8 @@ const cheerio = require('cheerio');
 const cached = require('./cache');
 const utils = require('./utils');
 
-const baseUrl = 'https://supremecommunity.com';
+const baseCommunityUrl = 'https://supremecommunity.com';
+const baseSupremeUrl = 'http://supremenewyork.com';
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -14,12 +15,12 @@ app.use(function(req, res, next) {
 });
 
 async function get (url) {
-  const data = await request(`${baseUrl}${url}`);
+  const data = await request(url);
   return cheerio.load(data);
 }
 
 async function getDropList() {
-  const $ = await get('/season/latest/droplists/');
+  const $ = await get(`${baseCommunityUrl}/season/latest/droplists/`);
   return Array.from($('.block').map((i, x) => ({
     url: x.attribs.href,
     name: $(x).text().replace(/\s\s+/g, ' ').trim(),
@@ -27,11 +28,31 @@ async function getDropList() {
   })));
 }
 
+async function getSupremeCategories() {
+  const $ = await get(`${baseSupremeUrl}/shop/all`);
+  return Array.from($('#nav-categories a').map((i, x) => (
+    {
+      label: $(x).text().trim(),
+      name: x.attribs.href.substr(x.attribs.href.lastIndexOf('/') + 1),
+    })))
+    .filter(x => ['new', 'all'].indexOf(x.name) === -1);
+}
+
+async function getSupremeProducts(category) {
+  const $ = await get(`${baseSupremeUrl}/shop/all/${category}`);
+  return Array.from($('article').map((i, x) => ({
+    url: $(x).find('a')[0].attribs.href,
+    name: $($(x).find('h1')[0]).text(),
+    color: $($(x).find('p')[0]).text(),
+    soldOut: $(x).find('.sold_out_tag').length >= 1,
+  })));
+}
+
 async function getProducts(url) {
-  const $ = await get(url);
+  const $ = await get(baseCommunityUrl + url);
   const cards = $('.card');
   return Array.from(cards.map((i, card) => {
-    const imageUrl = `${baseUrl}/${$(card).find('img')[0].attribs.src}`;
+    const imageUrl = `${baseCommunityUrl}/${$(card).find('img')[0].attribs.src}`;
     let name = $($(card).find('.name')[0]).text().trim();
     const price = $($(card).find('.label-price')[0]).text().trim();
     const category = $($(card).find('.category')[0]).text().replace(/\s\s+/g, ' ').trim();
@@ -42,7 +63,23 @@ async function getProducts(url) {
 
 const getProductsCached = cached(getProducts, 10);
 const getDropsCached = cached(getDropList, 10);
+const getCategoriesCached = cached(getSupremeCategories, 0.10);
+const getSupremeProductsCached = cached(getSupremeProducts, 0.10);
 
+app.get('/categories', async (req, res) => {
+  res.json(await getCategoriesCached());
+});
+
+app.get('/categories/:name/products', async (req, res) => {
+  const name = req.params.name;
+  const categories = await getCategoriesCached();
+  const category = categories.find(x => x.name === name);
+  if (!category) {
+    return res.sendStatus(404);
+  }
+
+  return res.json(await getSupremeProductsCached(category.name));
+});
 
 app.get('/drops', async (req, res) => {
   res.json(await getDropsCached());
@@ -51,9 +88,9 @@ app.get('/drops', async (req, res) => {
 app.get('/drops/:slug/products/', async (req, res) => {
   const drops = await getDropsCached();
   const slug = req.params.slug;
-  if (!slug) res.status(404);
+  if (!slug) return res.sendStatus(404);
   const drop = drops.find(x => x.slug === slug);
-  if (!drop) res.status(404);
+  if (!drop) return res.sendStatus(404);
 
   return res.json(await getProductsCached(drop.url));
 });
